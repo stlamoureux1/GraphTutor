ab721faf4d75b32fe5b7dd9a6943af3c74368a67
spencerlamoureux <stlamoureux1@gmail.com>
2024-07-07 06:38:46 -0700

    deleted unused code

--- src/Model/SimpleGraphImpl.h
+++ src/Model/SimpleGraphImpl.h
@@ -53,49 +53,6 @@ void SimpleGraph<T>::RemoveNode(NodeId id) {
     }
 }
 
-/*
-// TBD Change to use node id
-template <class T>
-void SimpleGraph<T>::RemoveNode(Node<T> node) {
-    // Remove this node from nodeList
-    auto nodeIter = find_if(nodeList.begin(),
-                            nodeList.end(),
-                            [&](Node<T> otherNode) { 
-                                return otherNode.GetId() == node.GetId();
-                            }    
-                        );
-    if (nodeIter != nodeList.end()) {
-        nodeList.erase(nodeIter);
-    }
-
-    // Remove from edgeList all edges containing this node
-    auto edgeIter = remove_if(edgeList.begin(),
-                                edgeList.end(),
-                                [&](Edge<T>& otherEdge) {
-                                    return otherEdge.GetNodeIds().first == node.GetId()
-                                            || otherEdge.GetNodeIds().second == node.GetId();
-                                }
-                            );
-    edgeList.erase(edgeIter, edgeList.end());
-
-    // Update adjacency list
-    auto idIter = adjacencyList.find(node.GetId());
-    if (idIter != adjacencyList.end()) {
-        adjacencyList.erase(idIter);
-    }
-    for (auto& [key, val] : adjacencyList) {
-        auto entryIter = remove_if(val.begin(),
-                                    val.end(),
-                                    [&](auto& entry) { 
-                                        return entry.first == node.GetId(); 
-                                    }
-                                );
-        val.erase(entryIter, val.end());
-        
-    }
-}
-*/
-
 template <class T>
 void SimpleGraph<T>::AddEdge(Edge<T> edge) {
     auto edgeIter = find_if(edgeList.begin(),
