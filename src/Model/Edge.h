efc8d26b21478536cbc3379d58cecc6dac30aeab
spencerlamoureux <stlamoureux1@gmail.com>
2024-07-07 17:34:57 -0700

    added JSON IO

--- src/Model/Edge.h
+++ src/Model/Edge.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "Node.h"
+#include "nlohmann/json.hpp"
+
 
 using EdgeId = unsigned int;
+using json = nlohmann::json;
 
 template <class T>
 class Edge {
     public:
         Edge(EdgeId id, Node<T> node1, Node<T> node2);
+        Edge(json j);
         EdgeId GetId();
         std::pair<NodeId, NodeId> GetNodeIds();
+        json ToJSON();
     private:
         EdgeId m_id;
         NodeId m_node1Id;
@@ -18,6 +23,9 @@ class Edge {
 template <class T>
 Edge<T>::Edge(EdgeId id, Node<T> node1, Node<T> node2)
     : m_id(id), m_node1Id(node1.GetId()), m_node2Id(node2.GetId()) {}
+
+template <class T>
+Edge<T>::Edge(json j) : m_id(j["id"]), m_node1Id(j["node1_id"]), m_node2Id(j["node2_id"]) {}
 
 template <class T>
 EdgeId Edge<T>::GetId() {
@@ -33,4 +41,13 @@ template <class T>
 bool operator==(Edge<T> edge1, Edge<T> edge2) {
     return edge1.GetId() == edge2.GetId()
             && edge2.GetNodeIds() == edge2.GetNodeIds();
+}
+
+template <class T>
+json Edge<T>::ToJSON() {
+    json j;
+    j["id"] = GetId();
+    j["node1_id"] = m_node1Id;
+    j["node2_id"] = m_node2Id; 
+    return j;
 }
--- test/Model/EdgeTest.cpp
+++ test/Model/EdgeTest.cpp
@@ -17,3 +17,23 @@ TEST(EdgeTest, test2) {
     Edge<int> edge2(0, node1, node2);
     ASSERT_EQ(edge1, edge2);
 }
+
+TEST(EdgeTest, IOtest1) {
+    Node<int> node1(0, 0);
+    Node<int> node2(1, 1);
+    Edge<int> edge1(0, node1, node2);
+    json j = edge1.ToJSON();
+    ASSERT_EQ(j["id"], 0);
+    ASSERT_EQ(j["node1_id"], 0);
+    ASSERT_EQ(j["node2_id"], 1);
+}
+
+TEST(EdgeTest, IOtest2) {
+    json j;
+    j["id"] = 0;
+    j["node1_id"] = 0;
+    j["node2_id"] = 1;
+    Edge<int> edge(j);
+    ASSERT_EQ(edge.GetId(), 0);
+    ASSERT_THAT(edge.GetNodeIds(), Pair(0, 1));
+}
